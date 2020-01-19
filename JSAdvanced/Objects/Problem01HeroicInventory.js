b2a09307b4db894ecc71f6a9bbdebde51008ad3d
Stanislav <slavi044@abv.bg>
2020-01-19 23:44:59 +0200

    Refactoring

--- JSAdvanced/Objects/Problem01HeroicInventory.js
+++ JSAdvanced/Objects/Problem01HeroicInventory.js
@@ -28,3 +28,5 @@ function solve(input) {
 solve(['Isacc / 25 / Apple, GravityGun',
     'Derek / 12 / BarrelVest, DestructionSword',
     'Hes / 1 / Desolator, Sentinel, Antara']);
+
+solve(['Jake / 1000 / Gauss, HolidayGrenade']);
