112697baeb6cfe25f4cadd380dc5cc41aa1228d8
Stanislav <slavi044@abv.bg>
2020-01-29 01:52:04 +0200

    Add homework

--- JSAdvanced/DOM/02. Number-Convertor/solution.js
+++ JSAdvanced/DOM/02. Number-Convertor/solution.js
@@ -1,4 +1,24 @@
 function solve() {
+    let optionList = document.querySelectorAll("#selectMenuTo")[0];
+    let button = document.querySelector("button");
+    let input = document.querySelector('#input');
+    
 
-    //TODO...
+    optionList.innerHTML = `
+    <option select value=""></option>
+    <option value="hexadecimal">Hexadecimal</option>
+    <option value="binary">Binary</option>
+    `;
+
+    button.addEventListener('click', () => {
+        let result;
+        if (optionList.value === "binary") {
+            result = (Number(input.value)).toString(2);
+        } else {
+            result = Number(input.value).toString(16).toLocaleUpperCase();
+        }
+
+        document.getElementById('result').value = result;
+        
+    });  
 }
--- JSAdvanced/DOM/03. JavaScript-Quizz/solution.js
+++ JSAdvanced/DOM/03. JavaScript-Quizz/solution.js
@@ -1,3 +1,34 @@
 function solve() {
-  //TODO...
+  let quizzie = document.getElementById('quizzie');
+  let sections = document.getElementsByTagName('section');
+  let result = document.querySelector(".results-inner h1");
+
+  let correctAnswers = ['onclick', 'JSON.stringify()', 'A programming API for HTML and XML documents'];
+  let userAnswers = 0;
+  let currentStep = 0;
+
+  quizzie.addEventListener('click', function handler(e){
+    if (e.target.className === 'answer-text') {
+      sections[currentStep].style.display = "none";
+       
+      if (correctAnswers.includes(e.target.innerHTML)) {
+        userAnswers++;
+      } 
+
+      currentStep++
+
+      if (currentStep < correctAnswers.length) {
+        sections[currentStep].style.display = "block";
+      } 
+
+      if (currentStep === 3) {
+
+        quizzie.removeEventListener('click', handler)
+
+        document.querySelector("#results").style.display = "block";
+        result.innerHTML = correctAnswers.length === userAnswers ?
+         "You are recognized as top JavaScript fan!" : `You have ${userAnswers} right answers`;
+      }
+    }
+  });
 }
