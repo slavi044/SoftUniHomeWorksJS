c3bb3af8b3f34677531b711009c32756d0ed42fa
Stanislav <slavi044@abv.bg>
2020-02-01 14:22:05 +0200

    Add homework

--- JSAdvanced/DOM/06. Table-Search-Engine/solution.js
+++ JSAdvanced/DOM/06. Table-Search-Engine/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-   //TODO...
-}
+   let searchBtn = document.getElementById('searchBtn');
+   let searchField = document.getElementById('searchField');
+   
+   searchBtn.addEventListener('click', search);
+
+   function search() {
+      let table = Array.from(document.querySelectorAll('tbody > tr'));
+      let regex = new RegExp(searchField.value, 'gim');
+      table.map(e => {
+         e.classList.remove('select');
+         if (e.innerHTML.match(regex) !== null) { 
+            e.className = 'select';
+         }
+      });
+     searchField.value = '';
+   };
+};
