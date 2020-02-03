3610d77d841e5dff4a4f50155dd42f44dd088399
Stanislav <slavi044@abv.bg>
2020-02-03 14:20:25 +0200

    Add homework

--- JSAdvanced/DOM Manipulations/07. Locked-Profile/app.js
+++ JSAdvanced/DOM Manipulations/07. Locked-Profile/app.js
@@ -1,3 +1,27 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const $buttons = document.querySelectorAll('div#container main#main div.profile button');
+
+    [...$buttons]
+        .forEach((button) => {
+            button.addEventListener('click', (event) => {
+                // const parent = event.currentTarget.previousElementSibling;
+                // console.log(parent);
+
+                const divSibling = event.currentTarget.parentNode.children[9];
+                const selector = divSibling.id.split('HiddenFields')[0];
+                const $unlockInput = document.querySelector(`input[name="${selector + 'Locked'}"]`);
+
+                if (!$unlockInput.checked && button.textContent === "Show more") {
+                    divSibling.style.display = "block";
+                    button.textContent = "Hide it";
+                } else if (!$unlockInput.checked) {
+                    divSibling.style.display = "none";
+                    button.textContent = "Show more";
+                }
+            });
+        });
+
+
+
+
 }
