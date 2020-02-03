3415fcd417d8c7d14a21202263864813ca32ad9b
Stanislav <slavi044@abv.bg>
2020-02-03 16:56:45 +0200

    Add homework

--- JSAdvanced/DOM Manipulations/08. Encode-and-Decode-Messages/app.js
+++ JSAdvanced/DOM Manipulations/08. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,34 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
-}
+    const $divs = document.querySelectorAll('div#container main#main div');
+
+    const $textAreas = document.querySelectorAll('textarea');
+
+    $textAreas[0].nextElementSibling.addEventListener('click', encode);
+    $textAreas[1].nextElementSibling.addEventListener('click', decode);
+
+    function encode(params) {
+        const text = $textAreas[0]
+        .value
+        .split('')
+        .map((char) => {
+            return String.fromCharCode(char.charCodeAt(0) + 1);
+        })
+        .join(''); 
+        
+        $textAreas[0].value = '';
+        $textAreas[1].value = text;
+    };
+
+    function decode(params) {  
+        const text = $textAreas[1]
+        .value
+        .split('')
+        .map((char) => {
+            return String.fromCharCode(char.charCodeAt(0) - 1);
+        })
+        .join('');
+        
+        $textAreas[1].value = text;
+    }
+
+};
