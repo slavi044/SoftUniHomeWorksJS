e9c66ef2dcabeae7590284ee9543cf3980f6354c
Stanislav <slavi044@abv.bg>
2020-02-04 00:32:36 +0200

    Add homework

--- JSAdvanced/DOM Manipulations/06. Time-Converter/solution.js
+++ JSAdvanced/DOM Manipulations/06. Time-Converter/solution.js
@@ -1,4 +1,34 @@
 function attachEventsListeners() {
+    let buttons = document.querySelectorAll('input[type="button"]');
 
-    console.log('TODO:...');
+    for (const button of buttons) {
+        button.addEventListener('click', convert);
+    }
+    
+    function convert(event) {
+        let type = event.target.parentElement.children[1].id;
+        let value = +event.target.parentElement.children[1].value;
+
+        let seconds = 0;
+
+        if (type === "days") {
+            seconds = value * 24 * 60 * 60;
+        } else if (type === "hours") {
+            seconds = value * 60 * 60;
+        } else if (type === "minutes") {
+            seconds = value * 60;
+        } else if(type === "seconds") {
+            seconds = value;
+        }
+
+        let minutes = seconds / 60;
+        let hours = minutes / 60;
+        let days = hours / 24;
+
+        document.getElementById('days').value = days;
+        document.getElementById('hours').value = hours;
+        document.getElementById('minutes').value = minutes;
+        document.getElementById('seconds').value = seconds;
+    }
+
 }
