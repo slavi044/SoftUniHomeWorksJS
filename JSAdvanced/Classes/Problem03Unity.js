48fe8fea73fa998fff7aa1416c7d9c66f792b8c1
Stanislav <slavi044@abv.bg>
2020-02-06 01:45:06 +0200

    Add homework

---
+++ JSAdvanced/Classes/Problem03Unity.js
@@ -0,0 +1,39 @@
+class Rat {
+    constructor(name) {
+        this.name = name;
+        this.unitedRats = [];
+    };
+
+    unite(otherRat) {
+        if (otherRat instanceof Rat) {
+            this.unitedRats.push(otherRat);
+        }
+    };
+
+    getRats() {
+        return this.unitedRats;
+    };
+
+    toString() {
+        let string = `${this.name}\n`;
+        
+        for (const rat of this.unitedRats) {
+            string += `#${rat.name}\n`;
+        }
+
+        return string;
+    };
+};
+
+let firstRat = new Rat('Peter');
+console.log(firstRat.toString()); // Peter
+console.log(firstRat.getRats()); // []
+firstRat.unite(new Rat('George'));
+firstRat.unite(new Rat('Alex'));
+console.log(firstRat.getRats());
+// [ Rat { name: 'George', unitedRats: [] },
+// Rat { name: 'Alex', unitedRats: [] } ]
+console.log(firstRat.toString());
+// Peter
+// ##George
+// ##Alex
