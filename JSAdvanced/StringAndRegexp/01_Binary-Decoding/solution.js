f61880b9f0e55ef938cc1c241616a1114b7ab980
Stanislav <slavi044@abv.bg>
2020-02-13 23:24:38 +0200

    Add homework

--- JSAdvanced/StringAndRegexp/01_Binary-Decoding/solution.js
+++ JSAdvanced/StringAndRegexp/01_Binary-Decoding/solution.js
@@ -1,3 +1,23 @@
+//let input = '0100011011010111100100100000011011100110000101101101011001010010000001101001011100110010';
+
 function solve() {
-	//TODO...
+	let input = document.getElementById('input').value;
+	let resultRef = document.getElementById('resultOutput');
+
+	let num = input.split('').map(Number).reduce((acc, x) => acc + x, 0);
+	while (num > 9) {
+		num = num.toString().split('').map(Number).reduce((acc, x) => acc + x, 0);
+	}
+
+	let parsedNumber = input.slice(num, input.length - num);
+
+	let chars = [];
+	for (let i = 0; i < Math.ceil(parsedNumber.length / 8); i++) {
+		chars.push(String.fromCharCode(parseInt(parsedNumber.slice(i * 8, (i + 1) * 8), 2)));
+	}
+
+	resultRef.innerHTML = chars.filter(x => x.match(/[A-Za-z ]/)).join('');
 }
+
+solve();
+
