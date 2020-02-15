fc6a10eb2bacf767ece6c065e2c08669264bd248
Stanislav <slavi044@abv.bg>
2020-02-15 19:31:30 +0200

    Add model

---
+++ JSAdvanced/ExamPreparation/JSAdvanced26October2019/Problem02SkiResort/tests.js
@@ -0,0 +1,8 @@
+let SkiResort = require('./solution');
+let assert = require('chai').assert;
+
+describe('SkiResort', function () {
+    it('asd', function(){
+        assert.equal(true, true, 'adsd');
+    });
+});
