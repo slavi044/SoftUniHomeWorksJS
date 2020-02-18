d89ea770c20f1fd599099f668c1a29954c0784fe
Stanislav <slavi044@abv.bg>
2020-02-18 13:52:25 +0200

    Add homework

--- JSAdvanced/DOM/LabProblems/03. Growing Word/solution.js
+++ JSAdvanced/DOM/LabProblems/03. Growing Word/solution.js
@@ -1,4 +1,35 @@
 function growingWord() {
+  let changeButton = document.getElementsByTagName('button')[0];
+  let growingWord = document.getElementsByTagName('div')[1].getElementsByTagName('p')[0];
 
-  //TODO...
+  let colors = {
+    '': '#5B88BD',
+    blue: '8FF897',
+    green: '#A40014',
+    red: '#5B88BD'
+  };
+
+  growingWord.style.fontSize = newSize();
+  growingWord.style.color = newColor();
+
+  function newSize() {
+    let size = growingWord.style.fontSize;
+    let newSize = '';
+    
+    if (size === '') {
+      newSize = 2;
+    } else {
+      newSize = size.match(/\d+/gmi);
+      newSize *= 2;
+    }
+
+    return newSize + 'px';
+  }
+
+  function newColor() {
+    let currentColor = growingWord.style.color;
+    return colors[currentColor];
+  }
+
+
 }
