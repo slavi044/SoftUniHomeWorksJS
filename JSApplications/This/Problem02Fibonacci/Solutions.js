055331be0832a60dd89d5ae2d1658bbd3d3fe455
Stanislav <slavi044@abv.bg>
2020-02-25 16:01:00 +0200

    Add homework

---
+++ JSApplications/This/Problem02Fibonacci/Solutions.js
@@ -0,0 +1,16 @@
+function getFibonator() {
+    let previous = 0;
+    let current = 1;
+    return function () {
+        let result = previous + current;
+        previous = current;
+        current = result;
+
+        return previous
+    }
+}
+
+let fib = getFibonator();
+console.log(fib());
+console.log(fib());
+console.log(fib());
