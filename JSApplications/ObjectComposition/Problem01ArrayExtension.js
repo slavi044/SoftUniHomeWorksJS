a1306be54a8b544a2dcc43c48ca71c34ebb1791f
Stanislav <slavi044@abv.bg>
2020-02-28 00:25:50 +0200

    Add comment in code

--- JSApplications/ObjectComposition/Problem01ArrayExtension.js
+++ JSApplications/ObjectComposition/Problem01ArrayExtension.js
@@ -46,48 +46,48 @@ Array.prototype.average = function () {
 
 console.log(myArr.average());
 
-(function solve() {
-    Array.prototype.last = function () {
-        return this[this.length - 1];
-    }
+// (function solve() {   <--- This is for test in judge.softuni.bg
+//     Array.prototype.last = function () {
+//         return this[this.length - 1];
+//     }
     
-    Array.prototype.skip = function (n) {
-        let resultArr = [];
-        for (let index = n; index < this.length; index++) {
-            resultArr.push(this[index]);
+//     Array.prototype.skip = function (n) {
+//         let resultArr = [];
+//         for (let index = n; index < this.length; index++) {
+//             resultArr.push(this[index]);
     
-        }
+//         }
     
-        return resultArr;
-    }
+//         return resultArr;
+//     }
     
-    Array.prototype.take = function (n) {
-        let resultArr = [];
-        for (let index = 0; index < n; index++) {
-            resultArr.push(this[index]);
+//     Array.prototype.take = function (n) {
+//         let resultArr = [];
+//         for (let index = 0; index < n; index++) {
+//             resultArr.push(this[index]);
     
-        }
+//         }
     
-        return resultArr;
-    }
+//         return resultArr;
+//     }
     
-    Array.prototype.sum = function () {
-        let sum = 0;
-        for (let index = 0; index < this.length; index++) {
-            sum += Number(this[index]);
+//     Array.prototype.sum = function () {
+//         let sum = 0;
+//         for (let index = 0; index < this.length; index++) {
+//             sum += Number(this[index]);
             
-        }
+//         }
     
-        return sum;
-    }
+//         return sum;
+//     }
 
-    Array.prototype.average = function () {
-        let sum = 0;
-        for (let index = 0; index < this.length; index++) {
-            sum += Number(this[index]);
+//     Array.prototype.average = function () {
+//         let sum = 0;
+//         for (let index = 0; index < this.length; index++) {
+//             sum += Number(this[index]);
             
-        }
+//         }
     
-        return sum / this.length;
-    }
-}())
+//         return sum / this.length;
+//     }
+// }())
