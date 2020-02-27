20b21bc3b6a51f1bb02be5c9464ea053ac2a45da
Stanislav <slavi044@abv.bg>
2020-02-28 00:21:07 +0200

    Add homework

---
+++ JSApplications/ObjectComposition/Problem01ArrayExtension.js
@@ -0,0 +1,93 @@
+let myArr = [1, 2, 3];
+
+Array.prototype.last = function () {
+    return this[this.length - 1];
+}
+
+Array.prototype.skip = function (n) {
+    let resultArr = [];
+    for (let index = n; index < this.length; index++) {
+        resultArr.push(this[index]);
+
+    }
+
+    return resultArr;
+}
+
+Array.prototype.take = function (n) {
+    let resultArr = [];
+    for (let index = 0; index < n; index++) {
+        resultArr.push(this[index]);
+
+    }
+
+    return resultArr;
+}
+
+Array.prototype.sum = function () {
+    let sum = 0;
+    for (let index = 0; index < this.length; index++) {
+        sum += Number(this[index]);
+        
+    }
+
+    return sum;
+}
+
+Array.prototype.average = function () {
+    let sum = 0;
+    for (let index = 0; index < this.length; index++) {
+        sum += Number(this[index]);
+        
+    }
+
+    return sum / this.length;
+}
+
+console.log(myArr.average());
+
+(function solve() {
+    Array.prototype.last = function () {
+        return this[this.length - 1];
+    }
+    
+    Array.prototype.skip = function (n) {
+        let resultArr = [];
+        for (let index = n; index < this.length; index++) {
+            resultArr.push(this[index]);
+    
+        }
+    
+        return resultArr;
+    }
+    
+    Array.prototype.take = function (n) {
+        let resultArr = [];
+        for (let index = 0; index < n; index++) {
+            resultArr.push(this[index]);
+    
+        }
+    
+        return resultArr;
+    }
+    
+    Array.prototype.sum = function () {
+        let sum = 0;
+        for (let index = 0; index < this.length; index++) {
+            sum += Number(this[index]);
+            
+        }
+    
+        return sum;
+    }
+
+    Array.prototype.average = function () {
+        let sum = 0;
+        for (let index = 0; index < this.length; index++) {
+            sum += Number(this[index]);
+            
+        }
+    
+        return sum / this.length;
+    }
+}())
