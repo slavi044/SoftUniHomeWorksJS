24c5799832e8d2f1af2d07e54b4e1f513fd1c1af
Stanislav <slavi044@abv.bg>
2020-03-03 21:24:40 +0200

    repaired code

--- JSApplications/PrototypesAndInteritance/Problem02People.js
+++ JSApplications/PrototypesAndInteritance/Problem02People.js
@@ -6,66 +6,61 @@ function solve() {
             }
             this.name = name;
             this.age = age;
-            this._salary = 0;
+            this.salary = 0;
             this.tasks = [];
         }
-
-        get salary() {
-            return this._salary;
-        }
-
-        set salary(value) {
-            return this._salary = value;
-        }
-
+ 
         work() {
             let currentTask = this.tasks.shift();
             console.log(this.name + currentTask);
             this.tasks.push(currentTask);
         }
-
+ 
         collectSalary() {
-            console.log(`${this.name} received ${this.getSalary()} this month`);
+            console.log(`${this.name} received ${this.getSalary()} this month.`);
+        }
+       getSalary(){
+            return this.salary;
         }
     }
-
+ 
     class Junior extends Employee {
         constructor(name, age) {
             super(name, age);
             this.tasks.push(' is working on a simple task.')
         }
-
+ 
         getSalary() {
             return this.salary;
         }
     }
-
+ 
     class Senior extends Employee {
         constructor(name, age) {
             super(name, age);
             this.tasks.push(' is working on a complicated task.');
             this.tasks.push(' is taking time off work.');
-            this.tasks.push(' is supervising junior workers');
+            this.tasks.push(' is supervising junior workers.');
         }
-
+ 
         getSalary() {
             return this.salary;
         }
     }
-
+ 
     class Manager extends Employee {
         constructor(name, age) {
             super(name, age);
-            this.divident = 0;
+            this.dividend = 0;
             this.tasks.push(' scheduled a meeting.');
             this.tasks.push(' is preparing a quarterly report.');
         }
-
+ 
         getSalary() {
-            return this.salary + this.divident;
+            return this.salary + this.dividend;
         }
     }
-
+ 
     return {
         Employee,
         Junior,
