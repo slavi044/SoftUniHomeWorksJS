d5a8169e1dca3cb54e8ed9614891c382d336a940
Stanislav <slavi044@abv.bg>
2020-03-11 16:10:39 +0200

    Add homework

--- JSApplications/RESTServicesAndAJAX/02.BUS-SCHEDULE/app.js
+++ JSApplications/RESTServicesAndAJAX/02.BUS-SCHEDULE/app.js
@@ -1,11 +1,29 @@
 function solve() {
+    let infoField = document.querySelector('.info');
+    let btnDepart = document.getElementById('depart');
+    let btnArrive = document.getElementById('arrive');
+
+    let currentStop = '';
+    let nextStop = 'depot';
 
     function depart() {
-        console.log('Depart TODO...');
+        fetch(`https://judgetests.firebaseio.com/schedule/${nextStop}.json`)
+        .then(x => x.json())
+        .then(getNextStop)
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoField.textContent = `Arriving at ${currentStop}`;
+        btnDepart.disabled = false;
+        btnArrive.disabled = true;
+    }
+
+    function getNextStop({ name, next}) {
+        currentStop = name;
+        nextStop = next;
+        infoField.textContent = `Next stop ${name}`;
+        btnDepart.disabled = true;
+        btnArrive.disabled = false;
     }
 
     return {
