f2fb1315fac9f19cbca907572ad839e1d6680624
Stanislav <slavi044@abv.bg>
2020-03-11 19:41:56 +0200

    add not full solution

--- JSApplications/RESTServicesAndAJAX/03.PHONEBOOK/app.js
+++ JSApplications/RESTServicesAndAJAX/03.PHONEBOOK/app.js
@@ -1,5 +1,13 @@
 function attachEvents() {
-    console.log('TODO...');
+    let loadBtn = document.getElementById('btnLoad');
+
+    loadBtn.addEventListener('click', () => {
+        fetch('https://phonebook-nakov.firebaseio.com/phonebook.json')
+        .then(x => x.json())
+        .then(res => {
+            console.log(res)
+        })
+    })
 }
 
 attachEvents();
