02ebd780333b04e0fb195b347e9dae3256453467
Stanislav <slavi044@abv.bg>
2020-03-16 17:33:17 +0200

    Add solution

--- JSApplications/AsynchronousProgramming/01. FORECASTER/app.js
+++ JSApplications/AsynchronousProgramming/01. FORECASTER/app.js
@@ -4,8 +4,55 @@ function attachEvents() {
 
     const elements = {
         locationInput: document.querySelector('#location'),
-        button: document.querySelector('#submit')
+        button: document.querySelector('#submit'),
+        currentDiv: document.querySelector('#current'),
+        upcomingDiv: document.querySelector('#upcoming'),
+        forecastWrapper: document.querySelector("#forecast") 
     }
+
+    const weatherSymbols = {
+        's': '☀',
+        'p': '⛅',
+        'o': '☁',
+        'r': '☂',
+        'd': '°'
+    }
+
+    function createHTMLElement(tagName, className, textContent) {
+        let element = document.createElement(tagName);
+
+        if (className) {
+            element.classList.add(...className);
+        }
+
+        if (textContent) {
+            element.textContent = textContent;
+        }
+
+        return element;
+    }
+
+    function showWeatherLocation([todayData, upcomingData]) {
+        const { condition, high, low } = todayData.forecast;
+
+        let forecastsDiv = createHTMLElement('div', ['forecasts']);
+        let symbolSpan = createHTMLElement('span', ['condition', 'symbol'], weatherSymbols[condition[0].toLowerCase()]);
+        let conditionSpan = createHTMLElement('span', ['condition']);
+
+        let forecastFirstDataSpan = createHTMLElement('span', ['forecast-data'], todayData.name);
+        let forecastSecondDataSpan = createHTMLElement('span', ['forecast-data'], `${low}${weatherSymbols.d}/${high}${weatherSymbols.d}`);
+        let forecastThirthDataSpan = createHTMLElement('span', ['forecast-data'], condition);
+
+        conditionSpan.appendChild(forecastFirstDataSpan);
+        conditionSpan.appendChild(forecastSecondDataSpan);
+        conditionSpan.appendChild(forecastThirthDataSpan);
+
+        forecastsDiv.appendChild(symbolSpan);
+        forecastsDiv.appendChild(conditionSpan);
+
+        elements.currentDiv.appendChild(forecastsDiv);
+        elements.forecastWrapper.style.display = 'block';
+    };
 
     elements.button.addEventListener('click', getLocationValue);
 
@@ -25,10 +72,10 @@ function attachEvents() {
                     fetch(CURRENT_UPCOMING_URL).then(res => res.json())
                 ])
                     .then(showWeatherLocation)
-                    .catch((e) => console.log(e.message))
+                    //.catch((e) => console.log(e.message))
 
             })
-            .catch((e) => console.log(e.message))
+            //.catch((e) => console.log(e.message))
     };
 }
 
