f596095d53517b848f84d25198603ed592756bca
Stanislav <slavi044@abv.bg>
2020-03-19 01:50:58 +0200

    solivng

--- JSApplications/RemoteDatabases/BOOKS/app.js
+++ JSApplications/RemoteDatabases/BOOKS/app.js
@@ -1,7 +1,22 @@
-import { get } from './firebase-requests.js';
+import { get, createBook, updateBook, deleteBook } from './firebase-requests.js';
+
+let btnLoadBooks = document.getElementById('loadBooks');
+let btnSubmit = document.getElementById('Submit');
+let titelField = document.getElementById('title');
+let authorField = document.getElementById('author');
+let ISBNField = document.getElementById('isbn');
 
 function engine() {
-    get().then(console.log);
+    btnLoadBooks.addEventListener('click', (event) => {
+        event.preventDefault();
+        get().then(console.log);               
+    });
+
+    btnSubmit.addEventListener('click', (event) => {
+        event.preventDefault();
+        console.log('asdasd')
+        //TODO: Create book as obj with id, create book... 
+    });
 }
 
 engine();
--- JSApplications/RemoteDatabases/BOOKS/firebase-requests.js
+++ JSApplications/RemoteDatabases/BOOKS/firebase-requests.js
@@ -1,8 +1,25 @@
-const fireBaseUrl = 'https://remotedb-exercisebooks.firebaseio.com/'; // <-- Here paste URL from your database! :) 
+const fireBaseUrl = 'https://remotedb-exercisebooks.firebaseio.com/remotedb-exercisebooks.json'; // <-- Here paste URL from your database! :) 
 
 function get() {
-    return fetch(fireBaseUrl + 'remotedb-exercisebooks.json')
+    return fetch(fireBaseUrl)
         .then(res => res.json());
 }
 
-export { get }
+function createBook(body) {
+    fetch(fireBaseUrl, {
+        method: 'POST',
+        body: body
+    })
+}
+
+function updateBook(id, body) {
+    
+}
+
+function deleteBook(id) {
+    fetch(fireBaseUrl, {
+        method: 'DELETE'
+    })
+}
+
+export { get, createBook, updateBook, deleteBook}
