3717719e060783e09a4b05177f85ac9312c77cd9
Stanislav <slavi044@abv.bg>
2020-03-24 01:33:17 +0200

    Push ready app

--- JSApplications/Templating/03. Popular Monkeys/monkeysTemplate.js
+++ JSApplications/Templating/03. Popular Monkeys/monkeysTemplate.js
@@ -1,3 +1,25 @@
-$(() => {
-    // TODO
-})
+import { monkeys } from './monkeys.js'
+
+let div = document.getElementById('table');
+
+async function engine() {
+    let temp = await fetch('./monkeyTemplate.hbs').then((r) => r.text());
+    let template = Handlebars.compile(temp);
+    let result = template(  { monkeys } );
+    
+    div.innerHTML = result;
+
+    let buttons = document.getElementsByClassName('btnInfo');
+
+    for (const btn of buttons) {
+        btn.addEventListener('click', (e) => {
+            if (e.target.parentElement.children[3].style.display === 'none') {
+                e.target.parentElement.children[3].style.display = 'block'
+            } else {
+                e.target.parentElement.children[3].style.display = 'none'
+            }
+        });
+    }
+}
+
+engine();
