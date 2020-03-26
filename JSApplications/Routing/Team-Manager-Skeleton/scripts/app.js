094206b84d6732dfe8134f8c7bbba1adcc6aaf9d
Stanislav <slavi044@abv.bg>
2020-03-26 20:25:24 +0200

    Set up project

--- JSApplications/Routing/Team-Manager-Skeleton/scripts/app.js
+++ JSApplications/Routing/Team-Manager-Skeleton/scripts/app.js
@@ -1,1 +1,24 @@
- // TODO
+// initialize the application
+var app = Sammy('#main', function () {
+    // include a plugin
+    this.use('Handlebars', 'hbs');
+
+    // define a 'route'
+    this.get('#/', async function (context) {
+        // load some data
+        context.partials = {
+            header: await this.load('./templates/common/header.hbs'),
+            footer: await this.load('./templates/common/footer.hbs')
+        }
+
+        this.partial('./templates/home/home.hbs')
+    });
+});
+
+// start the application
+
+
+(() => {
+    app.run('#/');
+    
+})();
