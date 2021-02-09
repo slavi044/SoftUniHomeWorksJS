bdc3f1f2f03ebf5c3729c3877e30aed09ee7f391
slavi044 <slavi044@abv.bg>
2021-02-09 23:54:15 +0200

    Add exercises

--- JSAdvanced/00Exercises/04DomIntroduction/03. Accordion/accordion.js
+++ JSAdvanced/00Exercises/04DomIntroduction/03. Accordion/accordion.js
@@ -1,3 +1,14 @@
 function toggle() {
-    console.log('TODO:...');
+    let button = document.getElementsByClassName('button')[0];
+    let div = document.getElementById('extra');
+    
+    if (button.textContent === 'More') {
+        div.style.display = 'block';  
+        button.textContent = 'Less';
+
+    } else if (button.textContent === 'Less') {
+        div.style.display = 'none';  
+        button.textContent = 'More';
+
+    }
 }
--- JSAdvanced/00Exercises/04DomIntroduction/04. Search in List/search.js
+++ JSAdvanced/00Exercises/04DomIntroduction/04. Search in List/search.js
@@ -1,3 +1,22 @@
 function search() {
-   // TODO
+   let input = document.getElementById('searchText').value;
+   let towns = document.getElementById('towns').children;
+   let output = document.getElementById('result');
+
+   let counter = 0;
+
+   if (input !== '' && input !== null && input !== undefined && input !== ' ') {
+      for (let i = 0; i < towns.length; i++) {
+         if (towns[i].textContent.toLowerCase().includes(input.toLowerCase())) {
+            towns[i].style.fontWeight = 'bold';
+            towns[i].style.textDecoration = 'underline';
+            counter++;
+         } else {
+            towns[i].style.fontWeight = '';
+            towns[i].style.textDecoration = '';
+         }
+      }
+   }
+   
+   output.textContent = `"${counter}" matches found`;
 }
