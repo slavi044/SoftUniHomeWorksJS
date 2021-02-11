6f9e824ec47b4d97013e99d083695bea0c19ad55
slavi044 <slavi044@abv.bg>
2021-02-12 00:33:12 +0200

    Add exercises

--- JSAdvanced/00Exercises/04DomIntroduction/06. Format the Text/solution.js
+++ JSAdvanced/00Exercises/04DomIntroduction/06. Format the Text/solution.js
@@ -1,3 +1,35 @@
 function solve() {
-  //TODO
+  let input = document.getElementById('input').value.split('.');
+  let output = document.getElementById('output');
+  input.pop();
+
+  let outputArr = [];
+
+  for (let i = 0; i < input.length; i++) {
+    input[i] = input[i] + '.';    
+  }
+
+  while (input.length > 0) {
+    if (input.length >= 3) {
+      let str = input.shift() + input.shift() + input.shift();
+      outputArr.push(str);
+
+    }else if (input.length == 2) {
+      let str = input.shift() + input.shift();
+      outputArr.push(str);
+
+    } else if (input.length == 1) {
+      outputArr.push(input.shift());
+
+    }
+  }
+
+  for (let i = 0; i < outputArr.length; i++) {
+    let p = document.createElement('p');
+    p.innerHTML = outputArr[i];
+    output.appendChild(p);
+    
+  }
+
+  console.log(outputArr);
 }
