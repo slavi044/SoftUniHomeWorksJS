ec094d5e37f052a2b584a8342eb19b532a495302
slavi044 <slavi044@abv.bg>
2021-02-11 23:37:09 +0200

    Add exercises

--- JSAdvanced/00Exercises/04DomIntroduction/05. Table - Serach Engine/solution.js
+++ JSAdvanced/00Exercises/04DomIntroduction/05. Table - Serach Engine/solution.js
@@ -2,7 +2,21 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let students = document.getElementsByTagName('tbody')[0].children;
+      let input = document.getElementById('searchField').value;
+      
+      for (let i = 0; i < students.length; i++) {
+         let currentEl = students[i].children;
+         for (let j = 0; j <= 1; j++) {
+            if (currentEl[j].textContent.includes(input) && currentEl[j].textContent.includes(input) !== " ") {
+               students[i].className = 'select';
+                break;
+            } else {
+               students[i].classList.remove('select');
+            }
+            
+         }
+      }
 
    }
 }
