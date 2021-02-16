6f78008bc03bda62f5bace138aff466caf17cd89
slavi044 <slavi044@abv.bg>
2021-02-16 13:39:47 +0200

    Add exercises

--- JSAdvanced/00Exercises/05DomManipulationsAndEvents/04. Fill-Dropdown/dropdown.js
+++ JSAdvanced/00Exercises/05DomManipulationsAndEvents/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+    let itemText = document.getElementById('newItemText');
+    let itemValue = document.getElementById('newItemValue');
+    console.log(itemValue.value);
+    let menu = document.getElementById('menu');
+
+    let option = document.createElement('option');
+    option.textContent = itemText.value;
+    option.value = itemValue.value;
+
+    itemText.value = '';
+    itemValue.value = '';
+
+    menu.appendChild(option);
 }
--- JSAdvanced/00Exercises/05DomManipulationsAndEvents/05. Encode-and-Decode-Messages/app.js
+++ JSAdvanced/00Exercises/05DomManipulationsAndEvents/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,32 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let encodeButton = document.getElementsByTagName('button')[0];
+    let decodeButton = document.getElementsByTagName('button')[1];
+    let encodeTextArea = document.getElementsByTagName('textarea')[0];
+    let decodeTextArea = document.getElementsByTagName('textarea')[1];
+    let message = '';
+
+    encodeButton.addEventListener('click', () => {
+        let arr = [];
+        for (let i = 0; i < encodeTextArea.value.length; i++) {
+            arr.push(String.fromCharCode(encodeTextArea.value.charCodeAt(i) + 1));
+            console.log(String.fromCharCode(encodeTextArea.value.charCodeAt(i) + 1));
+        }
+        
+        encodeTextArea.value = '';
+        message = arr.join('');
+        decodeTextArea.value = message;
+    });
+    
+    
+    decodeButton.addEventListener('click', () => {
+        console.log(message);
+        let arr = [];
+        for (let i = 0; i < message.length; i++) {
+            arr.push(String.fromCharCode(message.charCodeAt(i) - 1));
+            console.log(String.fromCharCode(encodeTextArea.value.charCodeAt(i) + 1));
+        }
+
+        decodeTextArea.value = arr.join('');
+        message = '';
+    });
 }
