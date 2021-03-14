336280d094fe1f4fe71b8494f1d87fadb532b9ff
Stanislav <slavi044@abv.bg>
2021-03-15 00:47:51 +0200

    Prepare Problem03

---
+++ JSAdvanced/00Exercises/07Exam/Problem03/tests.js
@@ -0,0 +1,9 @@
+let PizzUni = require("./PizzaPlace.js");
+let assert = require("chai").assert;
+let expect = require("chai").expect;
+
+describe("test1", function () {
+   it("test", function(){
+    expect(5, "5 = 5").to.be.equal(5);
+   }); 
+});
