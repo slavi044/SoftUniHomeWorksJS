1f66cad62242535fffaf330907a7c76b7217f4eb
slavi044 <slavi044@abv.bg>
2021-03-29 00:24:58 +0300

    Add exercises

--- JSApplications/00Exercises/02RestServicesAndAJAX/03.PHONEBOOK/app.js
+++ JSApplications/00Exercises/02RestServicesAndAJAX/03.PHONEBOOK/app.js
@@ -1,5 +1,61 @@
 function attachEvents() {
-    console.log('TODO...');
+    let personField = document.getElementById('person');
+    let phoneField = document.getElementById('phone');
+    let createBtn = document.getElementById('btnCreate');
+    let loadBtn = document.getElementById('btnLoad');
+    let phoneBook = document.getElementById('phonebook');
+
+    const url = `https://phonebook-nakov.firebaseio.com/phonebook.json`;
+
+    loadBtn.addEventListener('click', loadPersons);
+    createBtn.addEventListener('click', createPerson);
+
+    function loadPersons() {
+        phoneBook.innerHTML = '';
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                Object.entries(data).forEach(([id, person]) => {
+                    let li = document.createElement('li');
+                    li.textContent = `${person.person}: ${person.phone}`;
+
+                    let deleteBtn = document.createElement('button');
+                    deleteBtn.textContent = 'Delete';
+                    deleteBtn.addEventListener('click', deletePerson);
+                    deleteBtn.value = id;
+
+                    li.appendChild(deleteBtn);
+                    phoneBook.appendChild(li);
+                });
+            });
+
+    }
+
+    function deletePerson(event) {
+        const headers = { method: 'DELETE' };
+
+        fetch(`https://phonebook-nakov.firebaseio.com/phonebook/${this.value}.json`, headers)
+            .then(loadPersons);
+    }
+
+    function createPerson() {
+        let headers = {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify({
+                person: personField.value,
+                phone: phoneField.value
+            })
+        }
+
+        fetch(url, headers)
+            .then(() => {
+                personField.value = '';
+                phoneField.value = '';
+                loadPersons();
+            });
+    }
 }
 
 attachEvents();
--- JSApplications/00Exercises/02RestServicesAndAJAX/04.MESSENGER/app.js
+++ JSApplications/00Exercises/02RestServicesAndAJAX/04.MESSENGER/app.js
@@ -1,5 +1,43 @@
 function attachEvents() {
-    console.log('TODO...');
+    let nameField = document.getElementById('author');
+    let messageField = document.getElementById('content');
+    let textarea = document.getElementById('messages');
+    let btnSend = document.getElementById('submit');
+    let btnRefresh = document.getElementById('refresh');
+
+    const url = 'https://rest-messanger.firebaseio.com/messanger.json';
+
+    btnRefresh.addEventListener('click', refresh);
+    btnSend.addEventListener('click', sendMsg);
+
+    function refresh() {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                Object.values(data)
+                    .forEach(({ author, content }) => {
+                        textarea.value += `${author}: ${content}\n`;
+                    });
+            });
+    }
+
+    function sendMsg() {
+        let headers = {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify({
+                author: nameField.value,
+                content: messageField.value
+            })
+        }
+
+        fetch(url, headers)
+            .then(() => {
+                refresh();
+                nameField.value = '';
+                messageField.value = '';
+            });
+    }
 }
 
 attachEvents();
