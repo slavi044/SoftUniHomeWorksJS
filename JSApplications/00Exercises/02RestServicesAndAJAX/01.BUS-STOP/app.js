aaca4fd68d0816e3344bc846c4450e0c8db59ef3
slavi044 <slavi044@abv.bg>
2021-03-28 16:21:20 +0300

    Add exercises

--- JSApplications/00Exercises/02RestServicesAndAJAX/01.BUS-STOP/app.js
+++ JSApplications/00Exercises/02RestServicesAndAJAX/01.BUS-STOP/app.js
@@ -1,3 +1,27 @@
 function getInfo() {
-    console.log("TODO...");
+    let inputField = document.getElementById('stopId');
+    let stopName = document.getElementById('stopName');
+    let buses = document.getElementById('buses');
+    let validBuses = ['1287', '1308', '1327', '2334'];
+
+    if (!validBuses.includes(inputField.value)) {
+        stopName.textContent = 'Error';
+        return;
+    }
+
+    const url = `https://judgetests.firebaseio.com/businfo/${inputField.value}.json`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            stopName.textContent = data.name;
+
+            Object.keys(data.buses).forEach(key => {
+                let li = document.createElement('li');
+                li.textContent = `Bus ${key} arrives in ${data.buses[key]}`;
+                buses.appendChild(li);
+            })
+        });
+
+    stopName.textContent = '';
 }
--- JSApplications/00Exercises/02RestServicesAndAJAX/02.BUS-SCHEDULE/app.js
+++ JSApplications/00Exercises/02RestServicesAndAJAX/02.BUS-SCHEDULE/app.js
@@ -1,11 +1,36 @@
 function solve() {
+    let departBtn = document.getElementById('depart');
+    let arriveBtn = document.getElementById('arrive');
+    let info = document.getElementById('info');
 
+    let nextStop = 'depot';
+    let currentStop = '';
+    
     function depart() {
-        console.log('Depart TODO...');
+        const url = `https://judgetests.firebaseio.com/schedule/${nextStop}.json`;
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                nextStop = data.next;
+                info.textContent = `Next stop ${data.name}`;
+                currentStop = data.name;
+                departBtn.disabled = true;
+                arriveBtn.disabled = false;
+            })
+            .catch(res => {
+                if (res.ok === false) {
+                    throw new Error(`${res.status} - ${res.statusText}`);
+                }
+                return res;
+            })
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        info.textContent = `Arriving at ${currentStop}`;
+        currentStop = nextStop;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
