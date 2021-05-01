38c180704e679405e4e4157172258168337007fc
slavi044 <slavi044@abv.bg>
2021-05-01 18:27:35 +0300

    Add exercise

--- JSApplications/00Exercises/04RemoteDatabaes/06. JS-Applications-Remote-Databases-Exercise-Resources/STUDENTS/app.js
+++ JSApplications/00Exercises/04RemoteDatabaes/06. JS-Applications-Remote-Databases-Exercise-Resources/STUDENTS/app.js
@@ -1,1 +1,24 @@
-console.log('TODO...');
+let table = document.getElementById('results').children[1];
+
+fetch('https://stuudentsrdb-default-rtdb.firebaseio.com/.json')
+    .then(res => res.json())
+    .then(res => {
+        let result = Object.entries(res)
+        for (let i = 0; i < result.length; i++) {
+            let tr = document.createElement('tr');
+            let thId = document.createElement('th');
+            let thFName = document.createElement('th');
+            let thLName = document.createElement('th');
+            let thFNumber = document.createElement('th');
+            let thGrade = document.createElement('th');
+
+            thId.textContent = result[i][1].Id;
+            thFName.textContent = result[i][1].FirstName;
+            thLName.textContent = result[i][1].LastName;
+            thFNumber.textContent = result[i][1].FacultyNumber;
+            thGrade.textContent = result[i][1].Grade;
+
+            tr.append(thId, thFName, thLName, thFNumber, thGrade);
+            table.appendChild(tr);
+        }
+    })
