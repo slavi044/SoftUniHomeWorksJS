e0741df68174b7ac0ae67a8a99d673146b4bd091
slavi044 <slavi044@abv.bg>
2021-05-04 13:29:35 +0300

    Add exercise

--- JSApplications/00Exercises/05Templating/02. HTTP Status Cats/template.js
+++ JSApplications/00Exercises/05Templating/02. HTTP Status Cats/template.js
@@ -1,8 +1,50 @@
-(() => {
-     renderCatTemplate();
+(async() => {
+    let div = document.getElementById('allCats');
+    let temp = await fetch('./template.hbs')
+        .then(res => res.text());
+    let template = Handlebars.compile(temp);
+    let result = template({ cats })
+    
+    div.innerHTML = result;
 
-     function renderCatTemplate() {
-         // TODO: Render cat template and attach events
-     }
- 
-})
+    let buttons = document.getElementsByTagName('button');
+
+    for (const btn of buttons) {
+        btn.addEventListener('click', (e)=> {
+            if (e.target.parentElement.children[1].style.display === 'none') {
+                e.target.parentElement.children[1].style.display = 'block';
+                e.target.innerHTML = "Hide status code";
+            } else {
+                e.target.parentElement.children[1].style.display = 'none';
+                e.target.innerHTML = "Show status code";
+            }
+        })
+    }
+})()
+
+  
+// let div = document.getElementById('allCats');
+
+// (async () => {
+//     let temp = await fetch('./catTemplate.hbs').then(r => r.text());
+//     let template = Handlebars.compile(temp);
+//     let result = template( {cats} );
+    
+//     div.innerHTML = result;
+
+//     let buttons = document.getElementsByTagName('button');
+
+//     for (const btn of buttons) {
+//         btn.addEventListener('click', (e) => {
+//             console.log();
+            
+//             if (e.target.parentElement.children[1].style.display === 'none') {
+//                 e.target.parentElement.children[1].style.display = 'block';
+//                 e.target.innerHTML = "Hide status code";
+//             } else {
+//                 e.target.parentElement.children[1].style.display = 'none';
+//                 e.target.innerHTML = "Show status code";
+//             }
+//         });
+//     }
+// })()
