f980283f9af0c6342e89ae9b048d857878abc688
slavi044 <slavi044@abv.bg>
2021-05-09 13:13:33 +0300

    Fix error

--- JSApplications/00Exercises/06Routing/Team-Manager-Skeleton/scripts/app.js
+++ JSApplications/00Exercises/06Routing/Team-Manager-Skeleton/scripts/app.js
@@ -1,1 +1,9 @@
- // TODO
+const router = Sammy('#main', function () {
+    this.get('#/home', function () {
+        this.partial('../templates/home/home.hbs');
+    });
+});
+
+(() => {
+    router.run('#/home');
+})();
