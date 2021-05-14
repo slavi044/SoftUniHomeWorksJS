ef8dfa1fd9459ff5020a71fa35e3812640a66ff3
slavi044 <slavi044@abv.bg>
2021-05-15 00:41:18 +0300

    Add exercise

--- JSApplications/00Exercises/06Routing/Team-Manager-Skeleton/scripts/app.js
+++ JSApplications/00Exercises/06Routing/Team-Manager-Skeleton/scripts/app.js
@@ -1,9 +1,46 @@
 const router = Sammy('#main', function () {
-    this.get('#/home', function () {
-        this.partial('../templates/home/home.hbs');
+    this.use('Handlebars', 'hbs');
+
+    this.get('/home', function (context) {
+        loadPartials(context).then(function () {
+            this.partial('./templates/home/home.hbs');
+        });
+    });
+
+    this.get('/login', function (context) {
+        this.loadPartials({
+            'header': './templates/common/header.hbs',
+            'footer': './templates/common/footer.hbs',
+            'loginForm': './templates/login/loginForm.hbs'
+        }).then(function () {
+            this.partial('./templates/login/loginPage.hbs');
+        });
+    });
+
+    this.get('/register', function (context) {
+        this.loadPartials({
+            'header': './templates/common/header.hbs',
+            'footer': './templates/common/footer.hbs',
+            'registerForm': './templates/register/registerForm.hbs'
+        }).then(function () {
+            this.partial('./templates/register/registerPage.hbs');
+        });
+    });
+
+    this.get('/about', function (context) {
+        loadPartials(context).then(function () {
+            this.partial('./templates/about/about.hbs');
+        });
     });
 });
 
 (() => {
-    router.run('#/home');
+    router.run('/home');
 })();
+
+function loadPartials(context) {
+    return context.loadPartials({
+        'header': './templates/common/header.hbs',
+        'footer': './templates/common/footer.hbs'
+    });
+}
